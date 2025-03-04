09ec7455afb7588ca0f3db7db9c3f728a038941d
Mark Du <dulongjiebackup@gmail.com>
2025-03-04 08:44:14 -0800

    init

---
+++ app.py
@@ -0,0 +1,17 @@
+from scipy import constants
+import scipy
+print(constants.liter)
+print(constants.pi)
+print(dir(constants))
+print(scipy.__version__)
+
+print(constants.kibi)
+print(constants.tera)
+
+print(constants.pound)
+print(constants.lb)
+
+print(constants.zero_Celsius)
+print(constants.degree_Fahrenheit)
+
+print(constants.horsepower)
