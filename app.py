56cb3a2c20eb5b2738f6f0aafb50ffdc214d963c
Mark Du <dulongjiebackup@gmail.com>
2025-03-10 10:49:00 -0700

    spatial data/matlab array

--- app.py
+++ app.py
@@ -1,18 +1,74 @@
-from scipy import constants
-import scipy
-print(constants.liter)
-print(constants.pi)
-print(dir(constants))
-print(scipy.__version__)
+from scipy.optimize import root
+from scipy.sparse.csgraph import connected_components, dijkstra, floyd_warshall, bellman_ford, depth_first_order
+from scipy.optimize import minimize
+from scipy.sparse import csr_matrix
+from scipy.spatial import Delaunay
+import numpy as np
+from math import cos
 
-print(constants.kibi)
-print(constants.tera)
+def eqn(x):
+    return x + cos(x)
 
-print(constants.pound)
-print(constants.lb)
+myroot = root(eqn, 0)
 
-print(constants.zero_Celsius)
-print(constants.degree_Fahrenheit)
+print(myroot.x)
 
-print(constants.horsepower)
-print(constants.inch)
+def min_bfgs(x):
+    return x**2 + x +2
+
+mymin = minimize(min_bfgs, 0, method='BFGS')
+print(mymin)
+
+arr = np.array([0,0,0,0,0,0,1,1,0,2])
+print(csr_matrix(arr))
+
+arr_2d = np.array([
+    [0,0,0],
+    [0,0,1],
+    [1,0,2]
+])
+print(csr_matrix(arr_2d).data)
+mat = csr_matrix(arr_2d)
+print("Convert to csc:\n", mat.tocsc())
+'''
+mat.sum_duplicates()
+print("Eliminating duplicates:\n",mat)
+mat.eliminate_zeros()
+print("After eliminate zeros:\n",mat)
+'''
+
+graph_arr = np.array([
+    [0,1,2],
+    [1,0,0],
+    [2,0,0],
+])
+
+csr_graph_arr = csr_matrix(graph_arr)
+
+print(csr_graph_arr)
+print(connected_components(csr_graph_arr))
+print(dijkstra(csr_graph_arr, return_predecessors=True, indices=0))
+print(floyd_warshall(csr_graph_arr, return_predecessors=True))
+print(bellman_ford(csr_graph_arr, return_predecessors=True, indices=0))
+print(depth_first_order(csr_graph_arr, 1))
+
+points = np.array([
+    [2,4],
+    [3,4],
+    [3,0],
+    [2,2],
+    [4,1]
+])
+
+'''
+simplices = Delaunay(points).simplices
+
+plt.triplot(points[:, 0], points[:,1], simplices)
+plt.scatter(points[:,0], points[:,1], color='r')
+
+plt.show()
+'''
+from scipy import io
+io.savemat('arr.mat', {'vec': arr})
+
+print(io.loadmat('arr.mat', squeeze_me=True)['vec'])
