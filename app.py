dea7178c230efb64fdda94691e49293b1911af5c
Mark Du <dulongjiebackup@gmail.com>
2025-03-10 11:38:25 -0700

    interpolation

--- app.py
+++ app.py
@@ -72,3 +72,33 @@ from scipy import io
 io.savemat('arr.mat', {'vec': arr})
 
 print(io.loadmat('arr.mat', squeeze_me=True)['vec'])
+
+'''
+from scipy.interpolate import interp1d
+
+xs = np.arange(10)
+ys = 2*xs + 1
+
+interp_func = interp1d(xs,ys)
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+print(newarr)
+'''
+'''
+from scipy.interpolate import UnivariateSpline
+
+xs = np.arange(10)
+ys = xs**2 + np.sin(xs) + 1
+
+interp_func = UnivariateSpline(xs,ys)
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+print(newarr)
+
+from scipy.interpolate import Rbf
+
+xs = np.arange(10)
+ys = xs**2 + np.sin(xs) + 1
+
+interp_func = Rbf(xs,ys)
+newarr = interp_func(np.arange(2.1, 3, .1))
+print(newarr)
+'''
